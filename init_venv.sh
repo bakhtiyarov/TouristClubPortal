bbb077399bc864df1090f7535fb24927ed2292b4
Dmitriy Bakhtiyarov <bakhtiyarov.dima@yandex.ru>
2015-10-27 12:24:43 +0500

    create script for initialise virtual environment; add some directories in .gitignore

---
+++ init_venv.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+virtualenv venv -p /usr/bin/python3
+
+venv/bin/pip install flask
+venv/bin/pip install flask-login
+venv/bin/pip install flask-mail
+venv/bin/pip install flask-sqlalchemy
+venv/bin/pip install sqlalchemy-migrate
+venv/bin/pip install flask-whooshalchemy
+venv/bin/pip install flask-wtf
+venv/bin/pip install flask-babel
+venv/bin/pip install guess_language
+venv/bin/pip install flipflop
+venv/bin/pip install coverage
+venv/bin/pip install py-bcrypt
+
